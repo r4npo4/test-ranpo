159eafd6854d602a5a1646aae0a406bd94fcc1fb
r4npo4 <130885222+r4npo4@users.noreply.github.com>
2025-04-22 08:25:19 +0900

    Create app.py

---
+++ no_sql_injection/app/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template_string
+from pymongo import MongoClient
+
+app = Flask(__name__)
+client = MongoClient("mongodb://mongo:27017/")
+db = client["test"]
+users = db["users"]
+
+login_page = '''
+<h2>Login</h2>
+<form method="POST">
+  Username: <input name="username"><br>
+  Password: <input name="password"><br>
+  <input type="submit" value="Login">
+</form>
+<p>{{ result }}</p>
+'''
+
+@app.route('/', methods=["GET", "POST"])
+def login():
+    result = ""
+    if request.method == "POST":
+        if request.is_json:
+            data = request.get_json()
+            username = data.get('username')
+            password = data.get('password')
+        else:
+            username = request.form.get('username')
+            password = request.form.get('password')
+        
+        query = {"username": username, "password": password}
+        user = users.find_one(query)
+        
+        if user:
+            result = f"Welcome, {user['username']}!"
+        else:
+            result = "Login failed."
+
+    return render_template_string(login_page, result=result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
